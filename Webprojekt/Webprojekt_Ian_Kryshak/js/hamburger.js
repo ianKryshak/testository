68287edfc3157f596216af315594d166500685c5
ianKryshak <i.kryshak@yahoo.com>
2025-09-26 11:59:58 +0200

    Delete Webprojekt/Webprojekt_Ian_Kryshak directory

--- Webprojekt/Webprojekt_Ian_Kryshak/js/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const mainNav = document.getElementById('mainNav');
-
-hamburger.addEventListener('click', () => {
-    mainNav.classList.toggle('active');
-    hamburger.classList.toggle('open');
-});
